9d66709352b5b12476d06a94716ab9746489cd4b
Janith <janithrankelum0602@outlook.com>
2025-08-01 13:59:33 +0530

    Techstack

---
+++ src/sections/TechStack.jsx
@@ -0,0 +1,33 @@
+import React  from "react";
+import TitleHeader from "../components/TitleHeader";
+import { techStackIcons } from "../constants";
+
+const TechStack = () => {
+    return (
+        <div id="skills" className="flex-center section-padding">
+        <div className="W-full h-full md:px-10 px-5">
+        <div>
+        <TitleHeader 
+        title="My Preferred Tech Stack"
+        sub="🤝 The Skills I Bring to the Table"
+        />
+
+        <div className="tech-grid">
+        {techStackIcons.map((icon) => (
+            <div key={icon.name} className="card-border tech-card overflow-hidden group xl:rounded-full rounded-lg">
+                <div className="tech-card-animated-bg"/>
+            <div className="tech-card-content">
+                <div className="tech-icon-wrapper">
+                    <TechIcon model={icon} />
+                </div>
+            </div>
+            </div>
+        ))}
+        </div>
+      </div>
+    </div>
+    </div>
+    );
+};
+
+export default TechStack;
