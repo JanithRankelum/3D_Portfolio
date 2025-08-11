58a88ddd4d65e271a8ceb7d3bbba34f567970437
Janith <janithrankelum0602@outlook.com>
2025-08-11 13:55:06 +0530

    project 3

--- src/sections/ShowcaseSection.jsx
+++ src/sections/ShowcaseSection.jsx
@@ -78,7 +78,7 @@ const AppShowcase = () => {
               <div className="image-wrapper bg-[#FFE7EB]">
                 <img src="/images/project3.png" alt="YC Directory App" />
               </div>
-              <h2>YC Directory - A Startup Showcase App</h2>
+              <h2>E - Commerce Platform Web App</h2>
             </div>
           </div>
         </div>
